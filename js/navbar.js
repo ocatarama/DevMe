b83823048a7ab3d27134a28b59503fc23f8c5128
Ovi CT <catarama.ovidiu66@gmail.com>
2022-03-02 17:13:45 +0200

    update navbar mobile

--- js/navbar.js
+++ js/navbar.js
@@ -4,4 +4,16 @@ function mobileToggle(x) {
     navB = document.getElementById("n-btns");
 
     navB.style.display = navB.style.display == "block" ? "none" : "block"; 
-  }
+}
+
+addEventListener("resize", function() {
+    navB = document.getElementById("n-btns");
+    x = document.getElementById("menuB");
+    if(this.screen.width > 650)
+      navB.style.display = "none";
+    else
+    {
+      if(x.classList.contains("change"))
+        navB.style.display = "block";
+    }
+});
