14d55fc630daa91bf5b049a7a5126ccb5de37568
Ovi CT <catarama.ovidiu66@gmail.com>
2022-03-02 16:57:30 +0200

    update acasa

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,7 @@
 function mobileToggle(x) {
     x.classList.toggle("change");
 
-    navB = document.getElementById("n-btn");
+    navB = document.getElementById("n-btns");
 
     navB.style.display = navB.style.display == "block" ? "none" : "block"; 
   }
