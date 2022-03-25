162a4727c2ef3cc08cc72e8e75ff44e524f0ab4b
Ovi CT <catarama.ovidiu66@gmail.com>
2022-03-25 18:49:58 +0200

    add animations

--- js/info.js
+++ js/info.js
@@ -1,11 +1,88 @@
+/* ===================== WHAT IS DEVME TITLE ===================== */
+var executed3 = 0;
 document.addEventListener('scroll', () => {
-    var x = document.getElementById("howToTitle"); // titlul
+    var x = document.getElementById("whatIs");
+    var offset = 0.0;
+ 
+    if((window.pageYOffset >= x.offsetTop - 600) && executed3 == 0)
+    {
+        executed3 = 1;
+        setInterval(function(){
+            offset += 0.1;  
+            x.style.opacity = offset;
+        }, 50);    
+
+    }
+}, false);
+
+/* ===================== WHAT IS DEVME TEXT ===================== */
+document.addEventListener('scroll', () => {
+    var x = document.getElementById("whatAbout");
+    var offset = 0.0;
+ 
+    if((window.pageYOffset >= x.offsetTop - 600))
+    {
+        x.style.transform="translateX(0)";
+    }
+}, false);
+
+/* ===================== WHERE, WHEN ===================== */
+document.addEventListener('scroll', () => {
+    var x = document.getElementById("iSigns");
+
+    var where = document.getElementById("iWhere");
+    var when = document.getElementById("iWhen");
+
+    if(window.pageYOffset >= x.offsetTop - 500)
+    {
+        where.style.transform = "translateX(0)";
+        when.style.transform = "translateX(0)";
+    }
+}, false);
+
+/* ===================== INSTAGRAM IMAGES ===================== */
+var executed1 = 0;
+document.addEventListener('scroll', () => {
+    var x = document.getElementById("imgs");
+    var offset = 0.0;
+ 
+    if((window.pageYOffset >= x.offsetTop - 400) && executed1 == 0)
+    {
+        executed1 = 1;
+        setInterval(function(){
+            offset += 0.1;  
+            x.style.opacity = offset;
+        }, 50);    
+
+    }
+}, false);
+
+/* ===================== HOW TO HACK YOUR SUMMER ===================== */
+var executed2 = 0;
+document.addEventListener('scroll', () => {
+    var x = document.getElementById("howto-title");
+    var offset = 0.0;
+ 
+    if((window.pageYOffset >= x.offsetTop - 400) && executed2 == 0)
+    {
+        executed2 = 1;
+        setInterval(function(){
+            offset += 0.1;  
+            x.style.opacity = offset;
+        }, 50);    
+
+    }
+}, false);
+
+/* ===================== HOW TO HACK YOUR SUMMER ===================== */
+document.addEventListener('scroll', () => {
+    var x = document.getElementById("howTo"); // containerul
 
     var step1 = document.getElementById("step1");
     var step2 = document.getElementById("step2");
     var step3 = document.getElementById("step3");
 
-    if (window.pageYOffset >= x.offsetTop - 100)
+    if (window.pageYOffset >= x.offsetTop - 150)
     {
         step1.style.transform = "translateX(0)";
     }
@@ -13,8 +90,8 @@ document.addEventListener('scroll', () => {
     {
         step2.style.transform = "translateX(0)";
     }
-    if (window.pageYOffset >= x.offsetTop + 100)
+    if (window.pageYOffset >= x.offsetTop + 200)
     {
         step3.style.transform = "translateX(0)";
     }
-});
+}, false);
